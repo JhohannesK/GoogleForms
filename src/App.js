d4f494022e12b3866cecc63e75554570377f5245
JhohannesK <Jdomeh77@gmail.com>
2022-06-13 21:04:20 +0000

    resized it

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-
 import './App.css';
 import Footer from './components/Footer';
 import Layout from './components/Layout';
 
 function App() {
-  return (
-    <div className="h-screen flex flex-col justify-center items-center ">
-      <Layout />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="h-screen flex flex-col justify-center items-center">
+			<Layout />
+			<Footer />
+		</div>
+	);
 }
-
 export default App;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,23 +1,22 @@
-import React from 'react'
-import SignIn from './pages/signInpage'
-
+import React from 'react';
+import SignIn from './pages/signInpage';
 
 const Layout = () => {
-      return (
-            <div className='flex flex-col items-center justify-center gap-y-5 text-center w-full px-12 md:border md:w-[35rem] rounded-xl md:pt-16'>
-                  <div className='flex text-3xl font-medium text-center pointer-events-none'>
-                        <p className='text-blue-500'>G</p>
-                        <p className='text-red-600'>o</p>
-                        <p className='text-yellow-500'>o</p>
-                        <p className='text-blue-500'>g</p>
-                        <p className='text-green-700'>l</p>
-                        <p className='text-red-600'>e</p>
-                  </div>
-                  <span className='text-4xl font-light'>Sign in</span>
-                  <span className='text-xl'>to continue to Gmail</span>
-                  <SignIn />
-            </div>
-      )
-}
+	return (
+		<div className="flex flex-col items-center justify-center gap-y-5 text-center w-full px-12 md:border md:w-[30rem] rounded-xl md:pt-14">
+			<div className="flex text-2xl font-medium text-center pointer-events-none">
+				<p className="text-blue-500">G</p>
+				<p className="text-red-600">o</p>
+				<p className="text-yellow-500">o</p>
+				<p className="text-blue-500">g</p>
+				<p className="text-green-700">l</p>
+				<p className="text-red-600">e</p>
+			</div>
+			<span className="text-3xl font-light">Sign in</span>
+			<span className="text-lg">to continue to Gmail</span>
+			<SignIn />
+		</div>
+	);
+};
 
-export default Layout
+export default Layout;
